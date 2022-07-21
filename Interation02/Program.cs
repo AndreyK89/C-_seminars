fa36337c40732ccb3f1f3bac15b4f08bf15a7279
Andrey_K <kaa89job@yandex.ru>
2022-07-22 02:09:33 +0500

    Added Interation 02

---
+++ Interation02/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Прога выдает название дня недели по заданному числу.
+*/
+
+Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n==1) {
+    Console.WriteLine("Понедельник");
+} else if (n==2) {
+    Console.WriteLine("Вторник");
+} else if (n==3) {
+    Console.WriteLine("Среда");
+} else if (n==4) {
+    Console.WriteLine("Четверг");
+} else if (n==5) {
+    Console.WriteLine("Пятница");
+} else if (n==6) {
+    Console.WriteLine("Суббота");
+} else if (n==7) {
+    Console.WriteLine("Воскресение");
+} else {
+    Console.WriteLine("Нет такого дня!");
+}
