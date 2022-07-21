53b94923d3a6f750e1ac1c3e44cf595e2f2976fe
Andrey_K <kaa89job@yandex.ru>
2022-07-22 00:52:49 +0500

    added Task 00

---
+++ Task00/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter number: "); // Прога на вход принимает число и выдает его квадрат (умноженное само на себя).
+int num = int.Parse(Console.ReadLine());
+int res = num * num;
+Console.WriteLine("Result: " + res);
