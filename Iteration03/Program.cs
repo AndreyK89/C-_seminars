c686264c36fa10cfd9546d2d4764f09b1e344442
Andrey_K <kaa89job@yandex.ru>
2022-07-23 23:55:16 +0500

    Added Interation 03

--- Iteration03/Program.cs
+++ Iteration03/Program.cs
@@ -1,30 +1,19 @@
 ﻿/* 
+Выполнил Кошелев Андрей/ Группа GU | Разработчик | 2789 |
 Напишите программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел.
 */
 
  int a, b;
- 
-        Console.WriteLine("Введите два числа : ");
- 
-        a = Convert.ToInt32(Console.ReadLine());
- 
-        b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите первое число: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        b = int.Parse(Console.ReadLine());
  
         if (a > b)
- 
         {
- 
-            Console.WriteLine("{0} наибольшее число", a);
- 
+            Console.WriteLine("Максимальное число " + a);
         }
- 
         else
- 
         {
- 
-            Console.WriteLine("{0} наибольшее число ", b);
- 
-        }
- 
-        Console.ReadLine();
- 
+            Console.WriteLine("Максимальное число " + b);
+        }     
